abee834a92207d996b2a0a4ad6386f5ca7a8861c
Violet Bora Lee <learnerbr@gmail.com>
2020-05-29 20:00:10 +0900

    modify indentation

--- resistor-color-duo/resistor-color-duo.js
+++ resistor-color-duo/resistor-color-duo.js
@@ -2,7 +2,7 @@ export const decodedValue = (arr) => {
 
   const COLORS = ["black","brown","red","orange","yellow","green","blue","violet","grey","white"];
 
-	const colors = [...arr];
+  const colors = [...arr];
 
   return +`${COLORS.indexOf(colors[0])}${COLORS.indexOf(colors[1])}`;
 
