16603d1f065023a3414f090c7fc41ac68c077898
Violet Bora Lee <learnerbr@gmail.com>
2020-05-29 19:23:55 +0900

    solve register color duo
    - concept: strings, arrays

---
+++ resistor-color-duo/resistor-color-duo.js
@@ -0,0 +1,9 @@
+export const decodedValue = (arr) => {
+
+  const COLORS = ["black","brown","red","orange","yellow","green","blue","violet","grey","white"];
+
+	const colors = [...arr];
+
+  return +`${COLORS.indexOf(colors[0])}${COLORS.indexOf(colors[1])}`;
+
+};
