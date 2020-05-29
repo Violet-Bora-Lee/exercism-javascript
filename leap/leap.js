6d70b38812b3a41652d4009672df097d690fbf20
Violet Bora Lee <learnerbr@gmail.com>
2020-05-29 20:31:41 +0900

    solve leap using logical operators not if-else statements

--- leap/leap.js
+++ leap/leap.js
@@ -1,3 +1,7 @@
+// 서력 기원 연수가 4로 나누어 떨어지는 해는 윤년으로 한다.
+// 서력 기원 연수가 4, 100으로 나누어 떨어지는 해는 평년으로 한다.
+// 서력 기원 연수가 4, 100, 400으로 나누어 떨어지는 해는 윤년으로 둔다.
+
 export const isLeap = (year) => {
   if(year%4 === 0) {
     if (year%100 === 0) {
@@ -8,3 +12,7 @@ export const isLeap = (year) => {
     return false;
   }
 };
+
+// export const isLeap = (year) => {
+//   return year % 400 === 0 || (year % 100 !== 0 && year % 4 === 0);
+// };
